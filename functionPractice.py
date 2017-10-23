fb12144bcb02f4075981c4285e377369d7fa18ba
jamal <martian.hacked@gmail.com>
2017-10-23 18:47:18 +0500

    Default Arguments

--- functionPractice.py
+++ functionPractice.py
@@ -1,3 +1,5 @@
+# Arguments
+
 def my_function():
     print("this is function")
     print("second print")
@@ -11,3 +13,12 @@ def my_function_param(str1,str2):
     print(str2)
 
 my_function_param("First Argument","Second Argument")
+
+# Default Arguments
+
+def print_str_int(name = "Anyone" ,age = "Unknown"):
+    print( "My Name is " + name + " and my age is " + str(age))
+    # seperate them with comma
+    print("My Name is",name,"and my age is",age)
+
+print_str_int("Jamal")
