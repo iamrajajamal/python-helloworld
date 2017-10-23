827097863fa23a57f593b8eb3a1c06d41c0024b0
jamal <martian.hacked@gmail.com>
2017-10-23 18:52:42 +0500

    Keyword Argument

--- functionPractice.py
+++ functionPractice.py
@@ -9,6 +9,7 @@ my_function()
 
 
 def my_function_param(str1,str2):
+    print("By Argument")
     print(str1)
     print(str2)
 
@@ -17,8 +18,19 @@ my_function_param("First Argument","Second Argument")
 # Default Arguments
 
 def print_str_int(name = "Anyone" ,age = "Unknown"):
+    print("By Default Argument")
     print( "My Name is " + name + " and my age is " + str(age))
     # seperate them with comma
     print("My Name is",name,"and my age is",age)
 
 print_str_int("Jamal")
+
+# Keyword Arguments
+
+def print_str_int(name = "Anyone" ,age = "Unknown"):
+    print ("By Keyword Argument")
+    print( "My Name is " + name + " and my age is " + str(age))
+    # seperate them with comma
+    print("My Name is",name,"and my age is",age)
+
+print_str_int(age=24, name="Jamal")
