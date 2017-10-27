0dbfc1631daf55471ff6ceec343846dcf8ef0d53
jamal <martian.hacked@gmail.com>
2017-10-27 16:40:21 +0500

    Practice more

---
+++ pep.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from MyModule import foo, bar, foobar
+
+def my_function(one,two,
+                three,four,
+                five,six):
+    print("hello world")
+
+def second_function():
+    print("second funcrion")
+
+
+my_list = [1,2,
+           3,4,
+           5,6]
+
+print("Hello World", "Hi Everyone")
+
+check = True
+if check is True:
+    print("Check id True")
+
+
+#Dont do this
+func_one(); func_two; func_three;
