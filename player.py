e404baed9f28f8c8fa66fcc52544306f69a3d05f
jamal <martian.hacked@gmail.com>
2017-10-27 16:45:25 +0500

    stop while loop by break

--- player.py
+++ player.py
@@ -17,3 +17,19 @@ while playerHp > 0:
 
     if playerHp == 0:
         print("You have died.")
+
+
+#stop while loop by break keyword
+
+while playerHp > 0:
+    dmg = random.randrange(enemyAttackLow, enemyAttackHigh)
+    playerHp = playerHp - dmg
+
+    if playerHp <= 30:
+        playerHp = 30
+
+    print("Enemy Strikes for", dmg, "points of damage. Current hp is", playerHp)
+
+    if playerHp == 30:
+        print("You have low health. You've  been teleported to nearest location")
+        break
